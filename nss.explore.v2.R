7053b7ff29e51838732d870c039e95f3be334106
Jay Ulfelder <ulfelder@gmail.com>
2015-02-11 15:55:31 -0500

    Add new version of R script

---
+++ nss.explore.v2.R
@@ -0,0 +1,30 @@
+rm(list=ls(all=TRUE))
+setwd("c:/users/jay/documents/national-security-strategy")
+
+library(tm)
+library(SnowballC)
+
+# Read all texts into a single corpus; unsure of encoding here...
+NSS <- VCorpus(DirSource("C:/Users/Jay/Documents/national-security-strategy/texts", encoding = "utf-8"))
+
+# Create matrix of term counts by document
+NSS.tdm <- TermDocumentMatrix(NSS, control = list(stripWhitespace = TRUE, 
+                                                  removeNumbers = TRUE,
+                                                  removePunctuation = TRUE, 
+                                                  stopwords = TRUE, 
+                                                  tolower = TRUE,
+                                                  stemDocument = TRUE
+))
+
+# Make that a data frame with the terms as the first column
+NSS.df <- as.data.frame(cbind(row.names(NSS.matrix), NSS.matrix), stringsAsFactors = FALSE)
+
+# Give the columns in that data frame better names
+names(NSS.df) <- c("term", paste0("nss.", substr(list.files("texts/"), 1, 4)))
+
+# Fix the count columns to be read as numbers
+for (i in (2:length(names(NSS.df)))){ NSS.df[,i] <- as.numeric(NSS.df[,i]) }
+
+# Get rid of the row names
+row.names(NSS.df) <- NULL
+
