cff4761415988e4f2c432ccc6f4155101d7b68cc
Jay Ulfelder <ulfelder@gmail.com>
2015-02-15 18:50:42 -0500

    Update UI to explain normalization option

--- ui.R
+++ ui.R
@@ -11,21 +11,21 @@ shinyUI(fluidPage(
     
     mainPanel(
       p("This app lets you explore change over time in the national security concerns of the U.S.
-        government by plotting normalized term counts from all sixteen U.S. National Security Strategy
-        Reports, published irregularly by the White House from 1987 to 2015. The motivating assumption
+        government by plotting term counts from all 16  U.S. National Security Strategy Reports,
+        published irregularly by the White House from 1987 to 2015. The motivating assumption
         is that a term's prevalence in the report gives some indication of the salience of the entity or
         idea that term represents to the president's view of U.S. national security."),
-      p("Use the box on the right to input a term to plot. The plotting function will find all words
-        in the data set (a term-document matrix) that include that term; sum the counts of those words'
-        appearances by report; divide those sums by the total number of words in each report, and multiply
-        the result by 100 so it can be read as a percentage. For example, inputting 'afghan' will sum
-        counts by report for the terms 'afghan', 'afghans', afghanistan', and 'afghanistans'. The function
-        is not case sensitive."),
+      p("Use the text field on the right to input a term to plot, and check or uncheck the box to switch between normalized
+        and raw counts. The plotting function will find all words in the data set (a term-document matrix) that include that
+        term you type. For example, inputting 'afghan' will combine counts for the terms 'afghan', 'afghans', afghanistan',
+        and 'afghanistans'. The function is not case sensitive."),
       p("The data and scripts used to build this app can be found on GitHub, ",
         a(href = "https://github.com/ulfelder/national-security-strategy", "here"),
       ". This app was built by ",
         a(href = "https://dartthrowingchimp.wordpress.com/", "Jay Ulfelder"),
-      "."),
+      ". ",
+        a(href = "http://andybeger.com/author/andybeger/", "Andy Beger"),
+      "added the check box for normalization."),
       plotOutput("lineplot")
     )
     
