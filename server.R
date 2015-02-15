2b35f0672aa2a5e775c6e3cd3f58daef04520708
Jay Ulfelder <ulfelder@gmail.com>
2015-02-15 15:59:57 -0500

    Change Shiny app to normalized counts
    Switched function in Shiny app to use normalized instead of raw counts,
    and edited wrapper text.

--- server.R
+++ server.R
@@ -1,10 +1,20 @@
 NSS.tdm <- read.csv("data/nss.tdm.csv")
 
 plotWF <- function(word) {
-  t <- tolower(word)  # Make lower case just in case the user doesn't remember to do this.
-  z <- NSS.tdm[grep(t, NSS.tdm[,1]),2:17]  # Subset to rows for terms that contain 'word' & data columns
-  s <- colSums(z)  # Get sums by document (column) across those rows
-  yrs <- substr(names(NSS.tdm), 5, 8)[2:17]  # Get vector of years for labeling x-axis
+  
+  # Convert to lower case just in case the user doesn't remember to do this.
+  t <- tolower(word)
+
+  # Subset to rows for terms that contain 'word' & columns with the counts
+  z <- NSS.tdm[grep(t, NSS.tdm[,1]),2:17]
+
+  # Sum counts across rows and normalize by total words in each report
+  s <- (colSums(z)/colSums(NSS.tdm[,2:17])) * 100
+
+  # Get vector of years for labeling x-axis
+  yrs <- substr(names(NSS.tdm), 5, 8)[2:17]
+
+  # Make the plot
   par(cex.axis = 0.9)
   plot(x = 1:length(yrs), y = s, type = "l", lwd = 2, col = "firebrick2",
        xlab = "", ylab = "", axes = FALSE,
@@ -12,6 +22,7 @@ plotWF <- function(word) {
   axis(1, at = 1:length(yrs), labels = yrs, tick = FALSE, las = 2)
   axis(2, tick = FALSE, las = 2)
   abline(h = 0, lwd = 0.5, col = "gray")
+
 }
 
 shinyServer(function(input, output) {
--- ui.R
+++ ui.R
@@ -3,23 +3,25 @@ shinyUI(fluidPage(
   
   sidebarLayout(position = "right",
     
-    sidebarPanel(textInput("term", h4("Input a Term"), value = "soviet")),
+    sidebarPanel(textInput("term", h4("Input a Term"), value = "terror")),
     
     mainPanel(
-      p("This app lets you explore change over time in the national security concerns of the United States
-        government by plotting term counts from all sixteen U.S. National Security Strategy Reports,
-        published irregularly by the White House from 1987 to 2015."),
-      p("Use the box on the right to input a term to plot. The plotting function will find all terms
-        in the data set that include that term and will sum the counts of their appearances. For example,
-        inputting 'afghan' will combine counts by report for the terms 'afghan', 'afghans', afghanistan',
-        and 'afghanistans'. The function is not case sensitive."),
+      p("This app lets you explore change over time in the national security concerns of the U.S.
+        government by plotting normalized term counts from all sixteen U.S. National Security Strategy
+        Reports, published irregularly by the White House from 1987 to 2015. The motivating assumption
+        is that a term's prevalence in the report gives some indication of the salience of the entity or
+        idea that term represents to the president's view of U.S. national security."),
+      p("Use the box on the right to input a term to plot. The plotting function will find all words
+        in the data set (a term-document matrix) that include that term; sum the counts of those words'
+        appearances by report; divide those sums by the total number of words in each report, and multiply
+        the result by 100 so it can be read as a percentage. For example, inputting 'afghan' will sum
+        counts by report for the terms 'afghan', 'afghans', afghanistan', and 'afghanistans'. The function
+        is not case sensitive."),
       p("The data and scripts used to build this app can be found on GitHub, ",
         a(href = "https://github.com/ulfelder/national-security-strategy", "here"),
       ". This app was built by ",
         a(href = "https://dartthrowingchimp.wordpress.com/", "Jay Ulfelder"),
       "."),
-      br(),
-      br(),
       plotOutput("lineplot")
     )
     
